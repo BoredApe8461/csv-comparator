dec19ba79dc948fe2206cc32fe9d86c4489ec4ab
BoredApe8416 <letesinkelegesse@gmail.com>
2021-10-12 12:42:11 +0100

    Add `csv` and `serde` dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::error::Error;
+use std::io;
+use std::process;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Record {
+    name: String,
+    unoptimized_size: String,
+    optimized_size: String,
+}
+
+fn example() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(io::stdin());
+    for result in rdr.deserialize() {
+        // Notice that we need to provide a type hint for automatic
+        // deserialization.
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
