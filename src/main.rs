06e0e7cac276f6138c3bb4bf78bbca34253bd2b9
BoredApe8416 <letesinkelegesse@gmail.com>
2021-10-16 14:43:42 +0100

    Clean up error handling

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,38 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io;
-use std::process;
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Record {
+struct Row {
     name: String,
     unoptimized_size: f32,
     optimized_size: f32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct CsvComparitor {
     old_csv: HashMap<String, (f32, f32)>,
     new_csv: HashMap<String, (f32, f32)>,
 }
 
 impl CsvComparitor {
-    pub fn new() -> Self {
-        Self {
-            old_csv: Default::default(),
-            new_csv: Default::default(),
-        }
+    fn new() -> Self {
+        Default::default()
     }
 
-    pub fn write_old(&mut self, file: File) {
-        read_csv(&mut self.old_csv, file).unwrap();
+    fn write_old(&mut self, file: File) -> Result<()> {
+        read_csv(&mut self.old_csv, file)
     }
 
-    pub fn write_new(&mut self, file: File) {
-        read_csv(&mut self.new_csv, file).unwrap();
+    fn write_new(&mut self, file: File) -> Result<()> {
+        read_csv(&mut self.new_csv, file)
     }
 
-    pub fn get_diffs(&mut self) {
+    fn get_diffs(&mut self) -> Result<()> {
         let mut wtr = csv::Writer::from_writer(io::stdout());
 
         for (contract, (unopt_size_old, opt_size_old)) in &self.old_csv {
@@ -47,43 +45,43 @@ impl CsvComparitor {
             dbg!(opt_diff);
             dbg!(unopt_diff);
 
-            wtr.serialize(Record {
+            wtr.serialize(Row {
                 name: contract.to_string(),
                 unoptimized_size: unopt_diff,
                 optimized_size: opt_diff,
-            }).unwrap();
+            })?;
         }
+
+        Ok(())
     }
 }
 
-fn read_csv(map: &mut HashMap<String, (f32, f32)>, file: File) -> Result<(), Box<dyn Error>> {
+fn read_csv(map: &mut HashMap<String, (f32, f32)>, file: File) -> Result<()> {
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(false)
         .trim(csv::Trim::All)
         .from_reader(file);
 
     for result in rdr.deserialize() {
-        // Notice that we need to provide a type hint for automatic
-        // deserialization.
-        let record: Record = result?;
-        // println!("{:?}", record);
-        map.insert(record.name, (record.unoptimized_size, record.optimized_size));
+        let record: Row = result?;
+        map.insert(
+            record.name,
+            (record.unoptimized_size, record.optimized_size),
+        );
     }
+
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     let args: Vec<String> = std::env::args().collect();
-    println!("{:?}", args);
     let old = File::open(&args[1])?;
     let new = File::open(&args[2])?;
 
     let mut comparitor = CsvComparitor::new();
-    comparitor.write_old(old);
-    comparitor.write_new(new);
-    dbg!(&comparitor);
-
-    comparitor.get_diffs();
+    comparitor.write_old(old)?;
+    comparitor.write_new(new)?;
+    comparitor.get_diffs()?;
 
     // if let Err(err) = read_csv(f) {
     //     println!("error running example: {}", err);
